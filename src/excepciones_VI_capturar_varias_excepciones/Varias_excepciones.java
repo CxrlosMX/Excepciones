c33e455e825475d9bb3b890f568f365897aeb4f7
CxrlosMX <LuisCRendon131@gmail.com>
2021-08-18 23:24:33 -0500

    Varias excepciones en un solo programa

--- src/excepciones_VI_capturar_varias_excepciones/Varias_excepciones.java
+++ src/excepciones_VI_capturar_varias_excepciones/Varias_excepciones.java
@@ -3,7 +3,6 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package excepciones_VI_capturar_varias_excepciones;
 
 import javax.swing.JOptionPane;
@@ -15,20 +14,34 @@ import javax.swing.JOptionPane;
  * @Phone: 953-212-97-27
  * @Email: LuisCRendon131@gmail.com
  * @Date: 18/08/2021
- * 
+ *
  */
 public class Varias_excepciones {
     /*
-    Como capturar varias excepciones en un mismo programa
-    */
+     Como capturar varias excepciones en un mismo programa
+     */
+
     public static void main(String[] args) {
-        division();
+        try {
+            division();
+        } catch (ArithmeticException e) { //Excepcion Aritmetica
+            System.out.println("Estas dividiendo por cero "+e.getMessage());
+        }catch(NumberFormatException e){
+            System.out.println("No has introducido un numero entero");
+            System.out.println(e.getMessage()); //Nos informar acerca del error
+            System.out.println("Se ha generado un error de tipo: "+e.getClass()); //Nos devuelve la clase que ha generado el error
+        }
+
     }
 
     static void division() {
-       int n1=Integer.parseInt(JOptionPane.showInputDialog(null, "Introduce el dividendo","Dividendo", 1));
-       int n2=Integer.parseInt(JOptionPane.showInputDialog(null, "Introduce el divisor","Divisor", 1));
-        System.out.println("El resultado es: "+(n1/n2));
+        /*
+         Dentro de este programa existen dos tipo de excepciones uno de tipo Aritmetico y el otro de NumberFormat que sucede al introducir una letra
+         -Ambas excepciones heredan de RunTimeException por lo que son excepciones no controladas, no es una buena practica capturar los errores con Try-catch 
+         */
+        int n1 = Integer.parseInt(JOptionPane.showInputDialog(null, "Introduce el dividendo", "Dividendo", 1));
+        int n2 = Integer.parseInt(JOptionPane.showInputDialog(null, "Introduce el divisor", "Divisor", 1));
+        System.out.println("El resultado es: " + (n1 / n2));
     }
 
 }
